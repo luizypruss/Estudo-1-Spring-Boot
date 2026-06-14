36458766d58ae21d0f2c200336405922cbe360b1
Luizy <110498947+luizypruss@users.noreply.github.com>
2026-06-13 22:15:34 -0300

    Update Exercicio34ApplicationTests.java

--- Exercicio-34/src/test/java/com/example/Exercicio/_4/Exercicio34ApplicationTests.java
+++ Exercicio-34/src/test/java/com/example/Exercicio/_4/Exercicio34ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.Exercicio._4;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootTest
-class Exercicio34ApplicationTests {
+@SpringBootApplication
+@RestController
+public class Exercicio34Application {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Exercicio34Application.class, args);
+    }
 
+    // Endpoint  de exemplo do prof André
+    @GetMapping("/nome")
+    public String nome() {
+        return "João da Silva";
+    }
+
+    //  Endpoint CPF
+    @GetMapping("/cpf")
+    public String cpf() {
+        return "123.456.789-00";
+    }
+
+    //  Endpoint Endereço
+    @GetMapping("/endereco")
+    public String endereco() {
+        return "João da Silva - Rua das Flores, 123";
+    }
+
+    // Endpoint  Soma
+    @GetMapping("/soma")
+    public String soma(@RequestParam int a, @RequestParam int b) {
+        return "Resultado: " + (a + b);
+    }
 }
